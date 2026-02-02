ab9ae3569b6dffc324c5d949cad39d70e349dbe8
BTVNNHIEUV <nguyenkhanhhoang251003@gmail.com>
2026-02-02 13:45:08 +0700

    Update 30p.js

--- testin/30p.js
+++ testin/30p.js
@@ -1,27 +1,20 @@
-let Songuyen = Number(prompt("hãy nhập số nguyên dương: "));
-let count = 1;
-while(true){
-    Songuyen = Songuyen % 2;
-    if(Songuyen == 0){
-        console.log(`là số chẵn`);
+let n = Number(prompt("hãy nhập số nguyên dương: "));
+if (n % 2 === 0) {
+    console.log(`Số ${n} là số chẵn`);
+} else {
+    console.log(`Số ${n} là số lẻ`);
+}
+if (n > 0) {
+    console.log(`Số ${n} là số dương`);
+} else if (n < 0) {
+    console.log(`Số ${n} là số âm`);
+} else {
+    console.log(`Số ${n} bằng 0`);
+}
+if (n > 0) {
+    for (let number = 1; number <= n; number++) {
+        console.log(number);
     }
-    else if(Songuyen !== 0){
-        console.log(`là số lẻ`);
-    }
-    else if(Songuyen >= 0){
-        console.log(`là số dương`);
-    }
-    else if(Songuyen < 0){
-        console.log(`là số âm`);
-    }
-    else if(Songuyen > 0 && count <= Songuyen){
-        console.log(`dãy số của số nguyên là ${count + 1}`);
-        break;
-    }
-    else if(Songuyen <= 0){
-        console.log("Giá trị n không hợp lệ để tạo dãy số");
-    }
-    else{
-        console.log("Số không hợp lệ");
-    }
+} else {
+    console.log("Giá trị n không hợp lệ để tạo dãy số");
 }
